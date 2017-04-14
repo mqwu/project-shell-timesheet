76cd66028738d2b02b0444e56a45eb3a2d8c39c6
mqwu <send2mqwu@gmail.com>
2017-04-13 21:43:42 -0500

    Add read excel lib

--- src/run.R
+++ src/run.R
@@ -1,9 +1,15 @@
 .libPaths(unique(c("../lib",.libPaths())))
  
+packages <- c("shiny", "dplyr", "tidyr", "ggplot2", "readxl")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))  
+}
+ 
 library(shiny)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(readxl)
 
 if (file.exists ("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")) {
 	options(browser = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")
