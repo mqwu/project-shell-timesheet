e209d80cfb9e232c2086ba3e19f496b635258b40
mqwu <send2mqwu@gmail.com>
2017-04-13 21:45:13 -0500

    1. Read direct from excel file 2. Add WBS info

--- src/server.R
+++ src/server.R
@@ -6,18 +6,25 @@ shinyServer(function(input, output, session) {
 	  if (is.null(inFile))
 	  return(NULL)
 
-	  timesheet <- read.csv(inFile$datapath, header=TRUE)
-	  timesheet$Date2 <- as.Date(as.character(timesheet$Date), format="%m/%d/%Y")
-    
-	  timesheet.period <- filter(timesheet, Date2>= input$start, Date2 <= input$end, WeekDay!="Sat", WeekDay!="Sun", Project!="")
+	  file.rename(inFile$datapath,
+                  paste(inFile$datapath, ".xlsx", sep=""))
+
+	  wbs <- read_excel(paste(inFile$datapath, ".xlsx", sep=""), sheet=2, col_names=TRUE, na="")
+	  
+	  timesheet <- read_excel(paste(inFile$datapath, ".xlsx", sep=""), sheet=1, col_names=TRUE, na="")
+	  timesheet <- as.data.frame(timesheet)
+	  
+	  timesheet$Date2 <- as.Date(timesheet$Date, format="%m/%d/%Y")
+	  timesheet.period <- filter(timesheet, Date2>= input$start, Date2 <= input$end, WeekDay!="Sat", WeekDay!="Sun", Project!="") 
 	  timesheet.period$Mon <- format(timesheet.period$Date2, "%m")
+	  
 	  timesheet.show <- select(timesheet.period, Date, WeekDay, Project, Hours)
 	  
 	  proj.h <- timesheet.period %>% filter(grepl("^Proj|^proj", Project), Hours!='NA') %>% summarise(Project.Hours=sum(Hours))
 	  ovhd.h <- timesheet.period %>% filter(!grepl("^Proj|^proj", Project), Hours!='NA') %>% summarise(Overhead.Hours=sum(Hours))
 	  tot.h  <- timesheet.period %>% filter(Hours!='NA') %>% summarise(Total.Hours=sum(Hours))
 	  
-      	  overview <- cbind(proj.h, ovhd.h, tot.h)
+          overview <- cbind(proj.h, ovhd.h, tot.h)
 	  overview <- mutate(overview, Chargebility=Project.Hours/Total.Hours)
     
 	  # project hour summarized by month (for bar plot)
@@ -31,14 +38,17 @@ shinyServer(function(input, output, session) {
 					filter(Hours!='NA') %>%
 					group_by(Project) %>%
 					summarise(Hours=sum(Hours))
+	  details <- left_join(details, wbs, by="Project")
 			 
 	  # project hour summarized by weekday (for timesheet)
 	  proj.by.week <- timesheet.period %>%
-                        	group_by(WeekDay, Project) %>%
-                        	summarise(ProjHour=sum(Hours)) 
-          proj.by.week <- proj.by.week %>% 
-				spread(WeekDay, ProjHour) %>% 
-				select(Project, Mon, Tue, Wed, Thu, Fri)
+                        group_by(WeekDay, Project) %>%
+                        summarise(ProjHour=sum(Hours)) 
+						
+	  proj.by.week$Project <- as.character(proj.by.week$Project)
+      proj.by.week <- proj.by.week %>% 
+						spread(WeekDay, ProjHour, drop=FALSE) %>% 
+						select(Project, Mon, Tue, Wed, Thu, Fri)
 	  proj.by.week <- as.data.frame(proj.by.week)
 
 	return(list(timesheet.show, overview, details, proj.by.mon, proj.by.week))
@@ -51,17 +61,17 @@ shinyServer(function(input, output, session) {
   output$tableTimeperiod <- renderPrint({ 
     cat(paste0(input$start," to ",input$end))
   })
-  output$tableOverview <- renderTable({ dat()[[2]] })
-  output$tableSumByProj <- renderTable({ dat()[[3]] })
+  output$tableOverview <- renderTable({ dat()[[2]] }, digits=2)
+  output$tableSumByProj <- renderTable({ dat()[[3]] }, digits=0)
   
   output$plotProjByMonth <- renderPlot({
-    	ggplot(data=dat()[[4]], aes(x=Mon, y=ProjHour, fill=Project)) +
-      	geom_bar(stat="identity", position=position_dodge()) + scale_fill_brewer(palette = "Set1") +
-      	xlab("Month") + ylab("Hours")
+    ggplot(data=dat()[[4]], aes(x=Mon, y=ProjHour, fill=Project)) +
+      geom_bar(stat="identity", position=position_dodge()) + scale_fill_brewer(palette = "Set1") +
+      xlab("Month") + ylab("Hours")
   })
   
   # Tab3
-  output$tableTimesheet <- renderTable({ dat()[[5]] })
+  output$tableTimesheet <- renderTable({ dat()[[5]] }, digits=0)
 
   # Quit
   session$onSessionEnded(function(){ q() })
