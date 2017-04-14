10091b0a083d2dbc9de402342de499683b5b394c
mqwu <send2mqwu@gmail.com>
2017-04-13 21:47:10 -0500

    Read from excel directly

--- src/external/sidebar.R
+++ src/external/sidebar.R
@@ -1,14 +1,22 @@
 
 sidebarPanel(
 	wellPanel(
-       		fileInput('timesheet_dat', strong("Please load timeSheet (.csv)"),accept=c('.csv') )
+		p(strong("Step 1:"), br(),
+		  "Please follow the", 
+		  a("Timesheet Template",style="color:blue", href="../../charge15.xlsx", target="_blank"),
+		  "to record your time in excel.", br()
+		  ),
+		
+		p(strong("Step 2:"), br(),
+		  "Please load timeSheet (.xlsx)",
+       	  fileInput('timesheet_dat', NULL, accept=c('.xlsx'), multiple=TRUE)
+		 )		 
      	),
 
-     	wellPanel(
+    wellPanel(
 		p(strong("Please select time period for analysis")),
-		dateInput('start',label = 'Start date:',value = Sys.Date()),
-		dateInput('end',label = 'End date:',value = Sys.Date()+30)
+		dateInput('start',label = 'Start date:',value = Sys.Date()-30),
+		dateInput('end',label = 'End date:',value = Sys.Date())
      	)
+		
 )
-
-
